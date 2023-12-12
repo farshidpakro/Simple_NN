20609456df345df40caf5b86819330188314c951
farshid <farshidpakro@gmail.com>
2023-12-13 02:53:39 +0330

    first commit

---
+++ UploadPic/Form1.cs
@@ -0,0 +1,68 @@
+using System.Reflection;
+using System.Diagnostics;
+
+namespace UploadPic;
+
+public partial class Form1 : Form
+{
+    private Button button1;
+
+
+
+    public Form1()
+    {
+        InitializeComponent();
+        this.Size = new System.Drawing.Size(600, 400);
+        this.StartPosition = FormStartPosition.CenterScreen;
+        button1 = new Button();
+        this.Controls.Add(button1);
+        button1.Click += new EventHandler(button1_Click);
+        button1.Text = "Select Picture File";
+        button1.Location = new Point(70, 70);
+        button1.Size = new Size(500, 100);
+        button1.Location = new Point(40, 50);
+
+    }
+    private void button1_Click(object sender, EventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png"; // Filter to allow only image files
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            string selectedFile = openFileDialog.FileName;
+            string currentLocation = System.AppContext.BaseDirectory;
+            string fileName = currentLocation + "..\\..\\..\\..\\main.py " + selectedFile;
+
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo(@"python", fileName)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+            // p.WaitForExit();
+
+            string sample = "The predicted";
+            int index = output.IndexOf(sample);
+
+            if (index != -1)
+            {
+                output = output.Substring(index + sample.Length + 1);
+                MessageBox.Show(output);
+            }
+            else
+            {
+                MessageBox.Show("not match to any number");
+            }
+
+
+        }
+
+
+
+    }
+
+}
---
+++ main.py
@@ -0,0 +1,90 @@
+import sys
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input
+import pandas as pd
+import matplotlib.pyplot as plt
+import keras
+from keras import layers
+from keras.utils import to_categorical
+from keras import models
+from PIL import Image
+# import seaborn as sns
+import random
+import itertools
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+import os
+
+if len(sys.argv) < 2:
+    print("Usage: main.py <picture_file>")
+    sys.exit(1)
+
+picture_file = sys.argv[1]
+# picture_file = "C:\\Users\\farshid\\Desktop\\one.png"
+
+
+# Load the picture
+image = cv2.imread(picture_file, cv2.IMREAD_GRAYSCALE)
+
+# Resize the image to 28x28
+resized_image = cv2.resize(image, (28, 28))
+
+# Reshape the image to add a single channel dimension
+reshaped_image = resized_image.reshape(1, 28, 28, 1)
+
+# Normalize the image data
+normalized_image = reshaped_image / 255.0
+
+
+
+
+
+
+x_train =  pd.read_csv('mnist_train.csv')
+
+x_test =  pd.read_csv("mnist_test.csv")
+y_train = x_train.iloc[:, 0]
+y_test = x_test.iloc[:, 0]
+x_train = x_train.drop(x_train.columns[0], axis=1)
+x_test = x_test.drop(x_test.columns[0], axis=1)
+train_images = x_train
+train_labels = y_train
+test_images = x_test
+test_labels = y_test
+
+train_images =train_images.values.reshape(59999,28,28,1).astype('float32') / 255
+test_images =test_images.values.reshape(9999,28,28,1).astype('float32') / 255
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# Create a simple CNN model
+model = models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, y_train, epochs=10, validation_data=(test_images, y_test))
+model.evaluate(train_images, y_train)
+
+# Use the model to predict the image
+# predictions = model.predict(img_array.reshape(-1,28,28,1).astype('float32')/255)
+predictions = model.predict(normalized_image)
+# Get the predicted number
+predicted_numbers = np.argmax(predictions, axis=1)
+print ( y_test.to_numpy())
+
+print("The predicted numbers in the images are:", predicted_numbers)
